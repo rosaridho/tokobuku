7dc91b41376814fcdf53611bc0447689e6ef5d52
rosaridho <muhammadridhorosa@gmail.com>
2019-02-14 22:59:45 +0700

    ridhofitur2

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from .models import buku
 
 # Create your views here.
 def halamanHome(request):
-    return render(request,'test.html')
+    buku_obj = buku.objects.all()
+    return render(request, 'home.html', {'buku_obj':buku_obj})
